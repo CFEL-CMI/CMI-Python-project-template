9122b65084ac10b2e8a2465b1691b44660fd34d4
Jochen Küpper <jochen.kuepper@cfel.de>
2020-05-18 17:12:04 +0200

    implement some simple install and unit testing facility

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; fill-column: 120 -*-
+#
+# Copyright (C) 2020 Jochen Küpper <jochen.kuepper@cfel.de>
+
+
+import os
+from setuptools import setup
+
+extra_compile_args = []
+library_dirs = []
+
+long_description = """CMI Python template
+
+This is the installation and general build file of the CMI Python project template.
+
+Original author:    Jochen Küpper <jochen.kuepper@cfel.de>
+Current maintainer: Jochen Küpper <jochen.kuepper@cfel.de>
+"""
+
+
+setup(name="cmistark",
+      python_requires     = '>=3.5',
+      author              = "Jochen Küpper and the CFEL-CMI group",
+      author_email        = "jochen.kuepper@cfel.de",
+      maintainer          = "Jochen Küpper and the CFEL-CMI group",
+      maintainer_email    = "jochen.kuepper@cfel.de",
+      url                 = "https://github.com/CFEL-CMI/CMI-Python-project-template",
+      description         = "CMI Python-software template",
+      version             = "0.1.dev0",
+      long_description    = long_description,
+      license             = "GPL",
+      packages            = ['cmitemplate'],
+      scripts             = ['scripts/cmitemplate_calc'],
+      test_suite          = 'tests',
+      )
